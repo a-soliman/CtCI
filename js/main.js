f578d1f0329bc0fee96449ba11682d7ed67812b6
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-05-20 09:55:24 -0700

    solved isPrime

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
  // 1. isPrime
+function isPrime(n) {
+  for(var x =2; x*x <= n; x++) {
+    if(n % x === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log('isPrime : ' + isPrime(11));
+
+//=============================================
