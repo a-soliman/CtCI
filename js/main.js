da9f393195f1868034b3f5a5bd626cb4c6d3fb72
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-05-21 09:56:09 -0700

    solved nFactorial

--- js/main.js
+++ js/main.js
@@ -12,3 +12,14 @@ console.log('isPrime : ' + isPrime(11));
 
 //=============================================
 // 2. nFactorial
+
+function factorial(n) {
+  if(n < 0) { return -1; }
+  if(n === 0) { return 1; }
+  else {
+    return n * factorial(n-1);
+  }
+}
+console.log('factorial : ' + factorial(5))
+
+//=============================================
