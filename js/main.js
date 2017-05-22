6b6d054a7aed04d21f65d948c172a6361c7b15b1
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-05-22 09:57:03 -0700

    solved string permutation

--- js/main.js
+++ js/main.js
@@ -26,3 +26,30 @@ console.log('factorial : ' + factorial(5))
 
 
 // 3. string permutation
+
+
+// 3. string permutation
+function stringPermutation(str) {
+  var results = [];
+  
+  function permutations(subStr, prefix) {
+    //base case 
+    if(subStr <= 0) {
+      results.push(prefix);
+    }
+    
+    else {
+      for(var i = 0; i < subStr.length; i++) {
+        var rem = subStr.substring(0, i) + subStr.substring(i + 1);
+        permutations(rem, prefix + subStr.charAt(i));
+      }
+    }
+  }
+  permutations(str, '');
+  return results;
+}
+
+console.log('stringPermutation : ' + stringPermutation('abc'));
+
+/*
+======================================================================================
