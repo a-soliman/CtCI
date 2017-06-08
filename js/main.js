edc65dc1a1b2c646750a965dcdf4e95540aaec68
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-06-08 10:25:47 -0700

    coded 2 new linkedLists to test on

--- js/main.js
+++ js/main.js
@@ -672,3 +672,13 @@ console.log('deleteMiddleNode : ' + deleteMiddleNode(sLL, sLL.head.next.next).si
 ======================================================================================
   19. sumList: you have two numbers represented by a linked list, where each node contains a single digit. the digits are stored in reverse order, such that the 1's digit is the head of the list, write a function that adds the two numbers and returns the sum as a linked list
 */
+
+var linked1 = new LinkedList();
+linked1.addToHead(6);
+linked1.addToHead(1);
+linked1.addToHead(7);
+
+var linked2 = new LinkedList();
+linked2.addToHead(2);
+linked2.addToHead(9);
+linked2.addToHead(5);
