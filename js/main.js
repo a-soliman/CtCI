1ea549369bbe8fdb8e10ab5c394ff9f90c20d0ee
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-06-08 10:25:06 -0700

    sumList

--- js/main.js
+++ js/main.js
@@ -667,3 +667,8 @@ function deleteMiddleNode(linkedlist, node) {
 
 console.log('deleteMiddleNode : ' + deleteMiddleNode(sLL, sLL.head.next.next).size())
 
+
+/*
+======================================================================================
+  19. sumList: you have two numbers represented by a linked list, where each node contains a single digit. the digits are stored in reverse order, such that the 1's digit is the head of the list, write a function that adds the two numbers and returns the sum as a linked list
+*/
