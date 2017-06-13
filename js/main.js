b1b9b2510154aa9d8567c5644ffb1b52ad9cf9b0
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-06-13 14:46:34 -0700

    design a steak method in prototype

--- js/main.js
+++ js/main.js
@@ -924,3 +924,5 @@ console.log(stack)
 /*
 ======================================================================================
 
+  23. How would you design a stack which in addition to push and pop, has a function min. which returns the minimum element? push, pop and min should all operate in O(1)time
+*/
